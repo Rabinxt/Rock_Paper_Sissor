33f03531ebe7262b10b46bfdda536067d6ab574f
Rabin chhatkuli <140541319+Rabinxt@users.noreply.github.com>
2024-01-28 16:19:38 +0545

    Update rock_sissor_paper.py

--- rock_sissor_paper.py
+++ rock_sissor_paper.py
@@ -10,14 +10,18 @@ print("Write r for rock")
 player_inp = input("Enter (rock{r} , paper{p} ,sissor{s}) : ")
 print(f"Computer choose {cmp_choose}")
 print(f"YOu choose {player_inp}")
-
-if player_inp == "s" and cmp_choose == "paper" :
-    print("You won !!")
-elif player_inp == "p"  and cmp_choose == "rock" :
-    print("You won!!")
-elif player_inp == "r"  and cmp_choose == "sissor":
-    print("You Won!!")
-elif player_inp == cmp_choose :
-    print("It's tie")
-else:
-    print("You lost , Better luck next time!!")
+def function(player_inp , cmp_choose) :
+  if player_inp == "s" and cmp_choose == "paper" :
+      print("You won !!")
+  elif player_inp = " " :
+      print("empty box , Try again")
+      function(player_inp , cmp_choose)
+  elif player_inp == "p"  and cmp_choose == "rock" :
+      print("You won!!")
+  elif player_inp == "r"  and cmp_choose == "sissor":
+      print("You Won!!")
+  elif player_inp == cmp_choose :
+      print("It's tie")
+  else:
+      print("You lost , Better luck next time!!")
+function(player_inp , cmp_choose)
